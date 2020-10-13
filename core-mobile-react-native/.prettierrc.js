4d21ec82e16d973804e7ccfcef960faa26cf3d2d
Matheus Antonio <matheus.antonio208@gmail.com>
2020-10-13 13:30:34 -0300

    refactor tests layer and core react native

--- core-mobile-react-native/.prettierrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-};
--- core-mobile-react-native/src/middlewares/root-sagas.js
+++
@@ -1,7 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-import exampleSagas from './example-sagas';
-
-export default function* rootSaga() {
-  return yield all([exampleSagas]);
-}
